9789cf93be667f6e038cc113f6b1fb7601a095cd
korelw <dldldl9021@gmail.com>
2026-06-07 14:21:44 +0900

    fix: 변경

--- src/main/java/com/example/demo/Welfare.java
+++ src/main/java/com/example/demo/Welfare.java
@@ -13,33 +13,25 @@ public class Welfare {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
-
     @Column(length = 1000)
     private String purpose;
-
     private String agency;
     private String region;
-
     @Column(name = "info_url", columnDefinition = "TEXT")
     private String infoUrl;
-
     @Column(name = "site_link", columnDefinition = "TEXT")
     private String siteLink;
-
     @Column(name = "submit_link", columnDefinition = "TEXT")
     private String submitLink;
-
     private String keyword;
     private String income;
-
     @Column(name = "Sales")
     private String sales;
-
     private String rent;
     private String area;
-
     private String age;
     private String deadline;
+    private String gender;
+    private String special;
 }
